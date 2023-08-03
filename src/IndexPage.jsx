8f0734769f8633c810786d306cb617992d7e526f
Steven Kamanga <105992688+steven-kamanga@users.noreply.github.com>
2023-08-03 07:27:47 +0200

    Update IndexPage.jsx

--- src/IndexPage.jsx
+++ src/IndexPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { createClient } from "@sanity/client";
 
 const client = createClient({
-    projectId: "f3rzwmrv",
+    projectId: "",
     dataset: "production",
     apiVersion: "2023-03-31",
     useCdn: false,
